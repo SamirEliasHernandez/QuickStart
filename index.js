fb86e480a1f959218bf967287c0956c64df5969d
SamirEliasHernandez <samireliashdez@gmail.com>
2024-06-24 16:33:18 -0500

    subida inicial

---
+++ index.js
@@ -0,0 +1,16 @@
+let nombre = getElementById('op1');
+
+
+document.ready({
+	$("#calcular").click(function(){
+
+		var num1 = $('#op1').value();
+		var num2 = $('#op2').value();
+	})
+
+});
+
+var resultadoOperacion = num1 + num2;
+
+
+console.log(nombre);
